6c8bb69e739658bda55b7085cb21485697b4a559
jaymebryancreative <jaymebryancreative@gmail.com>
2026-08-29 09:13:21 -0400

    Use application shell as Reachwell entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/tokens.css'
 import './features/mission/mission.css'
-import { MissionMode } from './features/mission/MissionMode'
+import { AppShell } from './app/AppShell'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><MissionMode /></StrictMode>
+  <StrictMode><AppShell /></StrictMode>
 )
