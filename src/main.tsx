7b684f64e1d9b13b6197f3c064011ee3a8ae6da9
jaymebryancreative <jaymebryancreative@gmail.com>
2026-08-29 09:46:51 -0400

    feat(projects): load projects workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/tokens.css'
 import './features/mission/mission.css'
+import './features/projects/projects.css'
 import { AppShell } from './app/AppShell'
 
 createRoot(document.getElementById('root')!).render(
